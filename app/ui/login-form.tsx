d93004b83948e0185bd4b9792f40d59939dc0c5a
tahid02 <tanzim1463@gmail.com>
2024-06-04 18:56:44 +0600

    works so far

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Component() {
   const { data: session } = useSession();
@@ -15,7 +15,7 @@ export default function Component() {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signIn('cognito')}>Sign in</button>
     </>
   );
 }
